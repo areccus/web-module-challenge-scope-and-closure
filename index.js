5c3d6d5b8e97c9b557e12bc3ee506e1948aa5230
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2021-10-11 18:29:03 -0400

    doing task5

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  Counter1 is global and counter2 is bound to a function.
   2. Which of the two uses a closure? How can you tell?
-  
+  Counter1 is using a closure. It is using what's inside the counterMaker function.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     Counter1 would be preferable in situations where you wanted to save data or a memory. For example when you call counter1 it goes from 0 to 1 and if you call it again it goes from 1 to 2.
 */
 
 // counter1 code
@@ -63,7 +64,7 @@ NOTE: This will be a callback function for the tasks below
 */
 
 function inning(/*Code Here*/){
-    /*Code Here*/
+  return Math.floor(Math.random() * 3);
 }
 
 
@@ -81,8 +82,17 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB){
+  let homeScore = 0;
+  let awayScore = 0;
+  for(let i = 0; i < 9; i++) {
+    homeScore = homeScore + inningCB();
+    awayScore = awayScore + inningCB();
+  }
+  return {
+    Home: homeScore,
+    Away: awayScore
+  }
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +100,12 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+  //This one confused me. Was this just showing another cleaner way of doing task 3?
+function getInningScore(inningCB) {
+  return{
+    Home: inningCB(),
+    Away: inningCB()
+  }
 }
 
 
